580a13177320f29d80ab1c24ede48ffee42798c1
Gustavo Goldhardt <gustavo.goldhardt@gmail.com>
2023-07-08 17:52:02 -0300

    Changed app colors

--- app/src/main/java/me/goldhardt/woderful/presentation/theme/Color.kt
+++ app/src/main/java/me/goldhardt/woderful/presentation/theme/Color.kt
@@ -3,19 +3,32 @@ package me.goldhardt.woderful.presentation.theme
 import androidx.compose.ui.graphics.Color
 import androidx.wear.compose.material.Colors
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-val Red400 = Color(0xFFCF6679)
+val primary = Color(0xFF99CBFF)
+val onPrimary = Color(0xFF003354)
+val primaryContainer = Color(0xFF004A78)
+val secondary = Color(0xFFECC30A)
+val onSecondary = Color(0xFF3B2F00)
+val secondaryContainer = Color(0xFF564500)
+val error = Color(0xFFFFB4AB)
+val onError = Color(0xFF690005)
+val background = Color(0xFF1A1C1E)
+val onBackground = Color(0xFFE2E2E5)
+val onSurface = Color(0xFFE2E2E5)
+val surface = Color(0xFF42474E)
+val onSurfaceVariant = Color(0xFFC2C7CF)
 
 internal val wearColorPalette: Colors = Colors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200,
-    secondaryVariant = Teal200,
-    error = Red400,
-    onPrimary = Color.Black,
-    onSecondary = Color.Black,
-    onError = Color.Black
+    primary = primary,
+    primaryVariant = primaryContainer,
+    secondary = secondary,
+    secondaryVariant = secondaryContainer,
+    background = background,
+    surface = surface,
+    error  = error,
+    onPrimary = onPrimary,
+    onSecondary = onSecondary,
+    onBackground = onBackground,
+    onSurface = onSurface,
+    onSurfaceVariant = onSurfaceVariant,
+    onError = onError
 )

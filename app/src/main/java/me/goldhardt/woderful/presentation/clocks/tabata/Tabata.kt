55909907f0b9fbda664cb53dda5df90055b4fa24
gustavo <ggoldhardt@microsoft.com>
2023-12-01 19:44:46 -0300

    Tabata foundations

---
+++ app/src/main/java/me/goldhardt/woderful/presentation/clocks/tabata/Tabata.kt
@@ -0,0 +1,135 @@
+package me.goldhardt.woderful.presentation.clocks.tabata
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Picker
+import androidx.wear.compose.material.Scaffold
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.rememberPickerState
+import me.goldhardt.woderful.R
+import me.goldhardt.woderful.data.ServiceState
+import me.goldhardt.woderful.presentation.clocks.ExercisePermissions
+import me.goldhardt.woderful.presentation.clocks.ExercisePermissionsLauncher
+import me.goldhardt.woderful.presentation.clocks.ExerciseViewModel
+import me.goldhardt.woderful.presentation.component.ConfigurationButton
+import me.goldhardt.woderful.presentation.component.LoadingWorkout
+import me.goldhardt.woderful.presentation.component.PickerOptionText
+import me.goldhardt.woderful.presentation.component.RoundText
+
+/**
+ *  To do's
+ *
+ *  1. Maybe extract round picker to its own component??
+ */
+
+
+/**
+ * The flow of screens for the Tabata clock type
+ */
+internal sealed class TabataFlow {
+    object Permissions: TabataFlow()
+    object RoundsConfig : TabataFlow()
+    object WorkConfig : TabataFlow()
+    object RestConfig : TabataFlow()
+}
+
+@Composable
+fun TabataScreen(
+    viewModel: ExerciseViewModel = hiltViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    // Flow state
+    val initialScreen = if (ExercisePermissions.hasPermissions(LocalContext.current)) {
+        TabataFlow.RoundsConfig
+    } else {
+        TabataFlow.Permissions
+    }
+
+    var step: TabataFlow by remember { mutableStateOf(initialScreen) }
+
+    when (uiState.serviceState) {
+        is ServiceState.Connected -> {
+            when (step) {
+                TabataFlow.Permissions -> {
+                    ExercisePermissionsLauncher {
+                        step = TabataFlow.RoundsConfig
+                    }
+                }
+                TabataFlow.RoundsConfig -> {
+                    TabataRoundsConfig {
+                        step = TabataFlow.WorkConfig
+                    }
+                }
+                TabataFlow.WorkConfig -> Text("Work config")
+                TabataFlow.RestConfig -> Text("Rest config")
+            }
+        }
+        ServiceState.Disconnected -> {
+            LoadingWorkout()
+        }
+    }
+}
+
+
+private const val TABATA_MAX_ROUNDS = 20
+
+@Composable
+internal fun TabataRoundsConfig(
+    onConfirm: (Int) -> Unit
+) {
+    val state = rememberPickerState(initialNumberOfOptions = TABATA_MAX_ROUNDS)
+    Scaffold(
+        timeText = {
+            RoundText(stringResource(R.string.title_how_many_rounds))
+        }
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Spacer(modifier = Modifier.height(32.dp))
+            Picker(
+                modifier = Modifier.weight(1f),
+                state = state,
+                contentDescription = stringResource(R.string.title_how_many_rounds),
+            ) {
+                val currentRound = it + 1
+                PickerOptionText(
+                    selected = it == state.selectedOption,
+                    onSelected = {},
+                    text = "%d".format(currentRound),
+                    style = MaterialTheme.typography.display3
+                )
+            }
+            Spacer(modifier = Modifier.height(12.dp))
+            ConfigurationButton(
+                onConfirm = {
+                    onConfirm(state.selectedOption + 1)
+                },
+                icon = Icons.Filled.ArrowForward,
+                contentDescription = stringResource(R.string.action_confirm),
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+    }
+}
--- app/src/main/java/me/goldhardt/woderful/presentation/navigation/Navigation.kt
+++ app/src/main/java/me/goldhardt/woderful/presentation/navigation/Navigation.kt
@@ -8,12 +8,14 @@ import androidx.wear.compose.navigation.composable
 import me.goldhardt.woderful.data.model.ClockType
 import me.goldhardt.woderful.presentation.clocks.amrap.AmrapScreen
 import me.goldhardt.woderful.presentation.clocks.emom.EmomScreen
+import me.goldhardt.woderful.presentation.clocks.tabata.TabataScreen
 import me.goldhardt.woderful.presentation.history.History
 import me.goldhardt.woderful.presentation.home.HomeScreen
 import me.goldhardt.woderful.presentation.navigation.WODerfulScreens.AMRAP
 import me.goldhardt.woderful.presentation.navigation.WODerfulScreens.EMOM
 import me.goldhardt.woderful.presentation.navigation.WODerfulScreens.HISTORY
 import me.goldhardt.woderful.presentation.navigation.WODerfulScreens.HOME
+import me.goldhardt.woderful.presentation.navigation.WODerfulScreens.TABATA
 
 object WODerfulScreens {
     const val HOME = "home"
@@ -22,6 +24,7 @@ object WODerfulScreens {
     private const val CLOCK = "clock"
     const val AMRAP = "$CLOCK/amrap"
     const val EMOM = "$CLOCK/emom"
+    const val TABATA = "$CLOCK/tabata"
 
     // More Options
     const val HISTORY = "history"
@@ -47,6 +50,7 @@ fun MainNavigation(
                 when(type) {
                     ClockType.AMRAP -> navController.navigate(AMRAP)
                     ClockType.EMOM -> navController.navigate(EMOM)
+                    ClockType.TABATA -> navController.navigate(TABATA)
                     else -> {
                     }
                 }
@@ -57,6 +61,9 @@ fun MainNavigation(
         }
         composable(EMOM) {
             EmomScreen()
+        }
+        composable(TABATA) {
+            TabataScreen()
         }
         composable(HISTORY) {
             History()
